32bed86fedb008ecd80b0cf6df9cd4480a5828a7
lsanghoon <sou1.kiss1010@gmail.com>
2016-07-23 13:24:40 +0900

    sql 조건 변경

--- src/sql/user.sql
+++ src/sql/user.sql
@@ -14,8 +14,14 @@ CREATE TABLE USER (
   STU_NO VARCHAR(20) COMMENT '학번',
   DEPT VARCHAR(30) COMMENT '학과',
   GRADUATE VARCHAR(1) COMMENT '졸업여부',
-  APPRO VARCHAR(1) NOT NULL DEFAULT 'N' COMMENT '승인여부'
+  APPRO VARCHAR(1) NOT NULL DEFAULT '0' COMMENT '승인여부'
 );
+
+승인여부
+0 = 승인 대기 (준회원)
+1 = 승인 (정회원)
+2 = 운영자
+3 = 관리자
 
 insert into user (email, pwd, name, birth, gender, cphone, hphone, addr, pimg, job) 
 values ('aaa@aaa.aaa', 'aaaa', 'name', '1992-10-10', 'M', '00000000', '0000000', '서울시 ㅇㅇ구 ㅇㅇ동', 'abc.jpg', '직장인');
